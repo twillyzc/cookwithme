f8e4b04d4e88ec35edc4c7957a615b135732e71f
Kirill N <twillyz@icloud.com>
2020-06-06 01:59:15 +0700

    important styles

--- src/core/App/App.js
+++ src/core/App/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import { Route, Switch } from "react-router-dom";
 import Main from "../../screens/Main/Main";
 import Detail from "../../screens/Detail/Detail";
+import AboutPage from "../../screens/AboutPage/AboutPage";
 
 class App extends React.Component {
   render() {
@@ -10,6 +11,7 @@ class App extends React.Component {
       <Switch>
         <Route path="/" component={Main} exact />
         <Route path="/recipe/:id" component={Detail} />
+        <Route path="/about" component={AboutPage} />
       </Switch>
     );
   }
