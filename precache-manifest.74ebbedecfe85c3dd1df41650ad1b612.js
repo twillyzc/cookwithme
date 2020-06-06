13c9831799b54e46578f1058fda98028227e41ac
twillyzc <65772577+twillyzc@users.noreply.github.com>
2020-06-06 16:43:43 +0000

    Deploying to gh-pages from  @ ed3c42a95da94cc5fba58a0ecb8ebdf5042c7dee 🚀

---
+++ precache-manifest.74ebbedecfe85c3dd1df41650ad1b612.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d1ba6f3ae769ef6ec289db75b967a7c",
+    "url": "/cookwithme/index.html"
+  },
+  {
+    "revision": "543ab781862bfa396eb8",
+    "url": "/cookwithme/static/css/main.a769f7f9.chunk.css"
+  },
+  {
+    "revision": "1d6511904b4bb958d338",
+    "url": "/cookwithme/static/js/2.e4e54c18.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/cookwithme/static/js/2.e4e54c18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "543ab781862bfa396eb8",
+    "url": "/cookwithme/static/js/main.919c77be.chunk.js"
+  },
+  {
+    "revision": "11033ab707638860ae55",
+    "url": "/cookwithme/static/js/runtime-main.daf2b1dd.js"
+  }
+]);
