11276ccb6378c7a67ff066b9640c3878e9764e37
Kirill N <twillyz@icloud.com>
2020-06-07 19:56:00 +0700

    Loader sepparate component

---
+++ src/components/Loader/Loader.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Progress, Container } from "./Loader-styles";
+
+class Loader extends React.Component {
+  render() {
+    return (
+      <Container>
+        <Progress />
+      </Container>
+    );
+  }
+}
+
+export default Loader;
